f87a862341159c44681188db07577fe2c537ca72
Cenk Çokiçen <cenkc@windowslive.com>
2016-02-20 10:31:56 +0200

    Initial Commit

---
+++ Gun1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Gun1
+//
+//  Created by Cenk Çokiçen on 20/02/16.
+//  Copyright © 2016 Cenk Çokiçen. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
